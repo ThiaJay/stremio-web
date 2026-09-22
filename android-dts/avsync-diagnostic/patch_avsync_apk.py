ea30a6220de2222f2c7765d7947516920c2ca33c
TeeJay <3618451+ThiaJay@users.noreply.github.com>
2026-09-22 22:52:45 +0100

    Move AV sync sampling onto Android main thread

--- android-dts/avsync-diagnostic/patch_avsync_apk.py
+++ android-dts/avsync-diagnostic/patch_avsync_apk.py
@@ -80,12 +80,20 @@ probe.write_text(r'''.class public final Lcom/stremio/common/players/AvSyncProbe
 .implements Landroidx/media3/exoplayer/video/VideoFrameMetadataListener;
 
 .field private final player:Landroidx/media3/exoplayer/ExoPlayer;
+.field private final handler:Landroid/os/Handler;
 .field private count:I
 
 .method public constructor <init>(Landroidx/media3/exoplayer/ExoPlayer;)V
-    .locals 1
+    .locals 2
     invoke-direct {p0}, Ljava/lang/Object;-><init>()V
     iput-object p1, p0, Lcom/stremio/common/players/AvSyncProbe;->player:Landroidx/media3/exoplayer/ExoPlayer;
+
+    new-instance v0, Landroid/os/Handler;
+    invoke-static {}, Landroid/os/Looper;->getMainLooper()Landroid/os/Looper;
+    move-result-object v1
+    invoke-direct {v0, v1}, Landroid/os/Handler;-><init>(Landroid/os/Looper;)V
+    iput-object v0, p0, Lcom/stremio/common/players/AvSyncProbe;->handler:Landroid/os/Handler;
+
     const/4 v0, 0x0
     iput v0, p0, Lcom/stremio/common/players/AvSyncProbe;->count:I
     return-void
@@ -99,41 +107,73 @@ probe.write_text(r'''.class public final Lcom/stremio/common/players/AvSyncProbe
     return-object p0
 .end method
 
-
 .method public onVideoFrameAboutToBeRendered(JJLandroidx/media3/common/Format;Landroid/media/MediaFormat;)V
-    .locals 8
+    .locals 5
     iget v0, p0, Lcom/stremio/common/players/AvSyncProbe;->count:I
     add-int/lit8 v0, v0, 0x1
     iput v0, p0, Lcom/stremio/common/players/AvSyncProbe;->count:I
     rem-int/lit8 v0, v0, 0x1e
     if-nez v0, :done
 
-    iget-object v0, p0, Lcom/stremio/common/players/AvSyncProbe;->player:Landroidx/media3/exoplayer/ExoPlayer;
+    const-wide/16 v0, 0x3e8
+    div-long v0, p1, v0
+
+    new-instance v2, Lcom/stremio/common/players/AvSyncSample;
+    iget-object v3, p0, Lcom/stremio/common/players/AvSyncProbe;->player:Landroidx/media3/exoplayer/ExoPlayer;
+    invoke-direct {v2, v3, v0, v1}, Lcom/stremio/common/players/AvSyncSample;-><init>(Landroidx/media3/exoplayer/ExoPlayer;J)V
+
+    iget-object v3, p0, Lcom/stremio/common/players/AvSyncProbe;->handler:Landroid/os/Handler;
+    invoke-virtual {v3, v2}, Landroid/os/Handler;->post(Ljava/lang/Runnable;)Z
+    pop
+
+    :done
+    return-void
+.end method
+''')
+
+sample = companion.parent / "AvSyncSample.smali"
+sample.write_text(r'''.class public final Lcom/stremio/common/players/AvSyncSample;
+.super Ljava/lang/Object;
+.source "AvSyncSample.java"
+
+.implements Ljava/lang/Runnable;
+
+.field private final player:Landroidx/media3/exoplayer/ExoPlayer;
+.field private final framePtsMs:J
+
+.method public constructor <init>(Landroidx/media3/exoplayer/ExoPlayer;J)V
+    .locals 0
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+    iput-object p1, p0, Lcom/stremio/common/players/AvSyncSample;->player:Landroidx/media3/exoplayer/ExoPlayer;
+    iput-wide p2, p0, Lcom/stremio/common/players/AvSyncSample;->framePtsMs:J
+    return-void
+.end method
+
+.method public run()V
+    .locals 8
+    iget-object v0, p0, Lcom/stremio/common/players/AvSyncSample;->player:Landroidx/media3/exoplayer/ExoPlayer;
     invoke-interface {v0}, Landroidx/media3/exoplayer/ExoPlayer;->getCurrentPosition()J
     move-result-wide v0
 
-    const-wide/16 v2, 0x3e8
-    div-long v4, p1, v2
-    sub-long v6, v4, v0
+    iget-wide v2, p0, Lcom/stremio/common/players/AvSyncSample;->framePtsMs:J
+    sub-long v4, v2, v0
 
-    new-instance v2, Ljava/lang/StringBuilder;
-    invoke-direct {v2}, Ljava/lang/StringBuilder;-><init>()V
-    const-string v3, "positionMs="
-    invoke-virtual {v2, v3}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
-    invoke-virtual {v2, v0, v1}, Ljava/lang/StringBuilder;->append(J)Ljava/lang/StringBuilder;
+    new-instance v6, Ljava/lang/StringBuilder;
+    invoke-direct {v6}, Ljava/lang/StringBuilder;-><init>()V
+    const-string v7, "positionMs="
+    invoke-virtual {v6, v7}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+    invoke-virtual {v6, v0, v1}, Ljava/lang/StringBuilder;->append(J)Ljava/lang/StringBuilder;
     const-string v0, " framePtsMs="
-    invoke-virtual {v2, v0}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
-    invoke-virtual {v2, v4, v5}, Ljava/lang/StringBuilder;->append(J)Ljava/lang/StringBuilder;
+    invoke-virtual {v6, v0}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+    invoke-virtual {v6, v2, v3}, Ljava/lang/StringBuilder;->append(J)Ljava/lang/StringBuilder;
     const-string v0, " deltaMs="
-    invoke-virtual {v2, v0}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
-    invoke-virtual {v2, v6, v7}, Ljava/lang/StringBuilder;->append(J)Ljava/lang/StringBuilder;
-    invoke-virtual {v2}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
+    invoke-virtual {v6, v0}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+    invoke-virtual {v6, v4, v5}, Ljava/lang/StringBuilder;->append(J)Ljava/lang/StringBuilder;
+    invoke-virtual {v6}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
     move-result-object v0
 
     const-string v1, "STREMIO_AVSYNC"
     invoke-static {v1, v0}, Landroid/util/Log;->i(Ljava/lang/String;Ljava/lang/String;)I
-
-    :done
     return-void
 .end method
 ''')
@@ -142,3 +182,4 @@ print(f"patched {manifest}")
 print(f"patched {renderer_path}")
 print(f"patched {companion}")
 print(f"created {probe}")
+print(f"created {sample}")
